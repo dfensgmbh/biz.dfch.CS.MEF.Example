5a11c2d08463fe1aab2aa3b2d85447dcd9e133bc
Ronald Rink <ronald.rink@d-fens.net>
2015-06-30 11:26:59 +0200

    code optimisations

--- SimpleCalculator3/Program.cs
+++ SimpleCalculator3/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
-using System.Diagnostics;
 using System.IO;
 
 namespace SimpleCalculator
