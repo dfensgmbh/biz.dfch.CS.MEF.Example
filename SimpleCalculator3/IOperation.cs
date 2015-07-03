e3c52a7219aedde365ab5265650c1030b5a1f293
Ronald Rink <ronald.rink@d-fens.net>
2015-07-03 06:34:25 +0200

    added comment on naming convention for Metadata and ~Data interface

--- SimpleCalculator3/IOperation.cs
+++ SimpleCalculator3/IOperation.cs
@@ -3,6 +3,10 @@
  **/
 namespace SimpleCalculator
 {
+    // this interface defines how to import and export extensions
+    // in a real life project this interface definition would 
+    // probably be defined in a separate project so that both 
+    // exporter and importer can reference it
     public interface IOperation
     {
         int Operate(int left, int right);
--- SimpleCalculator3/IOperationData.cs
+++ SimpleCalculator3/IOperationData.cs
@@ -2,6 +2,14 @@ using System;
 
 namespace SimpleCalculator
 {
+    // this interface defines the data that will be imported 
+    // along with the interface
+    // keep in mind that the name MUST be suffixed with 'Data'
+    // by convention:
+    // IOperation -- > IOperationData
+    // in a real life project this interface definition would 
+    // probably be defined in a separate project so that both 
+    // exporter and importer can reference it
     public interface IOperationData
     {
         Char Symbol { get; }
